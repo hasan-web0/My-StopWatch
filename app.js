5f41d7e4aab19127eafaacd3ca033a57e5bead45
Hasan <121763739+hasan-web0@users.noreply.github.com>
2023-08-30 23:24:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+var hour = 0;
+var min = 0;
+var sec = 0;
+var msec = 0;
+var interval;
+
+var hourHeading = document.getElementById("hour");
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+
+function StartTimer()
+{
+    msec++;
+    msecHeading.innerHTML = msec;
+    if(msec>=100)
+    {
+        sec++;
+        secHeading.innerHTML = sec;
+        msec = 0;
+    }
+    else if(sec>=60)
+    {
+        min++;
+        minHeading.innerHTML = min;
+        sec = 0;
+    }
+    else if(min>=60)
+    {
+        hour++;
+        hourHeading.innerHTML = hour;
+        min = 0;
+    }
+}
+
+function start()
+{
+   interval = setInterval(StartTimer,10);
+}
+
+function pause()
+{
+  clearInterval(interval);  
+}
+
+function reset()
+{
+    hour = 0;
+    min = 0;
+    sec = 0;
+    msec = 0;
+    hourHeading.innerHTML = hour;
+    minHeading.innerHTML = min;
+    secHeading.innerHTML = sec;
+    msecHeading.innerHTML = msec;
+    pause();
+}
